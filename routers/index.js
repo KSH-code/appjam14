c1b8f8c1e1b5b458572d7eef628833db90b172e0
KSH-Code <tjdgnsqn133@gmail.com>
2017-12-16 21:14:25 +0900

    setting up routers

--- routers/index.js
+++ routers/index.js
@@ -1,4 +1,10 @@
 var app;
 module.exports = _app => {
     app = _app;
+    fs.readdir(`${__dirname}`, (e, fileList) => {
+        for(let fileName of fileList){
+            if(fileName == 'index.js') continue;
+            require(`./${fileName}`)(app);
+        }
+    });
 }
