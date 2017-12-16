bd3245aa3f3d96fc54e37c738af254e13ccb8380
KSH-Code <tjdgnsqn133@gmail.com>
2017-12-17 07:38:01 +0900

    ㅇ

--- controllers/generatePdf.js
+++ controllers/generatePdf.js
@@ -6,15 +6,18 @@ module.exports = (_app, _con) => {
 }
 
 module.exports.generatePdf = (req, res) => {
+    let count = rs[0].count;
     let { name, schoolName, id } = req.body;
-    let startHtml = 
-`
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="utf-8">
-    <title>Talk학 인증서</title>
-    <style>
+    con.query('select count(*) as count from `comments` where `writer` = ? and `check` = 1', [name], (e, rs) => {
+        // if(e) console.error(e), res.status(400).end();
+        let startHtml = 
+        `
+        <!DOCTYPE html>
+        <html lang="ko">
+        <head>
+        <meta charset="utf-8">
+        <title>Talk학 인증서</title>
+        <style>
         *{
             margin: 0; padding: 0; 
             font-family: "궁서";
@@ -47,12 +50,12 @@ module.exports.generatePdf = (req, res) => {
             letter-spacing: 1.2px;
             font-size: 25px;
         }
-    </style>
-</head>
-<body>
-    <div class="wrap">
+        </style>
+        </head>
+        <body>
+        <div class="wrap">
         <h1>Talk학 인증서</h1>
-        <p class="name">${name}</p>
+        <p class="name">총 ${count}건 완료<br>${name}</p>
         <p class="text">
             ${schoolName} 재학, 출신 ${name}님께서는 Talk학에서 1학기(6개월)동안
             훌륭한 답변율을 지원해주셨으며 많은 중, 고등학생에게 많은 배움을 공유해주셨기에
@@ -63,21 +66,20 @@ module.exports.generatePdf = (req, res) => {
                 ${new Date().toISOString().split("T")[0]} ${name}님꼐 인증서를 수여합니다.
             </span>    
         </p>
-    </div>
-</body>
-</html>
-`;
-    let dir = __dirname;
-    dir = dir.substr(0, dir.length - 12) + '/public/';
-    fs.mkdir(`${dir}${id}`, (e) =>{
-        if(fs.existsSync(`${dir}${id}/temp.html`)) fs.unlinkSync(`${dir}${id}/temp.html`);
-        fs.writeFile(`${dir}${id}/temp.html`, startHtml, (e) => {
-            if(e) res.status(400).end(), console.error(e);
-            else res.status(200).end()
+        </div>
+        </body>
+        </html>
+        `;
+        let dir = __dirname;
+        dir = dir.substr(0, dir.length - 12) + '/public/';
+        fs.mkdir(`${dir}${id}`, (e) =>{
+            if(fs.existsSync(`${dir}${id}/temp.html`)) fs.unlinkSync(`${dir}${id}/temp.html`);
+            fs.writeFile(`${dir}${id}/temp.html`, startHtml, (e) => {
+                if(e) res.status(400).end(), console.error(e);
+                else res.status(200).end()
+            });
         });
-    });
 
+    }
 }
-
-
 
