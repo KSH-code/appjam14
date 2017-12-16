f5e90e8774ab559df914257ca202623aa852704a
KSH-Code <tjdgnsqn133@gmail.com>
2017-12-16 23:16:28 +0900

    파일업로드 세팅

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(fileUpload());
 
 const routers = require('./routers/index')(app);
 const controllers = require('./controllers/index')(app);
