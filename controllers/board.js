b9b31668b21d202b4344cf29b4069f2e96756e27
KSH-Code <tjdgnsqn133@gmail.com>
2017-12-17 06:53:26 +0900

    과목 다시 살림

--- controllers/board.js
+++ controllers/board.js
@@ -14,12 +14,12 @@ module.exports.write = function(req, res) {
         error = true, error_msg = '잘못된 접근 입니다.';
     else if(content == undefined || !content.length)
         error = true, error_msg = '내용을 입력해주세요.';
-    // else if(subject == undefined || !subject.length)
-    //     error = true, error_msg = '과목을 입력해주세요.';
+    else if(subject == undefined || !subject.length)
+        error = true, error_msg = '과목을 입력해주세요.';
     
     if(error) return res.json({error, error_msg});
 
-    con.query('insert into `board` (`title`, `content`, `writer`, `subject`, `created_date`) values (?, ?, ?, "", now())', ['', content, writer, subject], (e, rs) => {
+    con.query('insert into `board` (`title`, `content`, `writer`, `subject`, `created_date`) values (?, ?, ?, ?, now())', ['', content, writer, subject], (e, rs) => {
         if(e) console.error(e), error = true, error_msg = '알 수 없는 오류!'
         else res.status(200).json({error}).end();
         if(img && !e){
